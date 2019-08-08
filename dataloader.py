268966193f6293e15d1670fb28ea7648be9697a1
Alisher <alisher.abdulkhaev@gmail.com>
2019-08-09 02:26:02 +0900

    train-val split input image normalization standard cnn architecture

--- dataloader.py
+++ dataloader.py
@@ -6,56 +6,83 @@ import pickle as pkl
 from PIL import Image
 import torch
 from torchvision import transforms
+from keras_preprocessing.image import ImageDataGenerator
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from nsml import DATASET_PATH
 
-def train_dataloader(input_size=128,
-                    batch_size=64,
-                    num_workers=0,
-                    ):
-    
-    image_dir = os.path.join(DATASET_PATH, 'train', 'train_data', 'images') 
-    train_label_path = os.path.join(DATASET_PATH, 'train', 'train_label') 
+
+def train_dataloader(input_size=128, batch_size=64, num_workers=0,):
+    image_dir = os.path.join(DATASET_PATH, 'train', 'train_data', 'images')
+    train_label_path_ = os.path.join(DATASET_PATH, 'train', 'train_label')
+    labels = np.load(train_label_path_)
     train_meta_path = os.path.join(DATASET_PATH, 'train', 'train_data', 'train_with_valid_tags.csv')
-    train_meta_data = pd.read_csv(train_meta_path, delimiter=',', header=0)
-        
-    dataloader = DataLoader(
-        AIRushDataset(image_dir, train_meta_data, label_path=train_label_path, 
-                      transform=transforms.Compose([transforms.Resize((input_size, input_size)), transforms.ToTensor()])),
+    train_meta_data_ = pd.read_csv(train_meta_path, delimiter=',', header=0)
+    val_num = int(train_meta_data_.shape[0]*0.1)
+
+    train_meta_data = train_meta_data_.iloc[val_num:, :]
+    val_meta_data = train_meta_data_.iloc[:val_num, :]
+
+    train_labels = labels[val_num:, :]
+    val_labels = labels[:val_num, :]
+
+    train_dataloader = DataLoader(
+        AIRushDataset(image_dir, train_meta_data, label_path=train_label_path_,
+                      transform=transforms.Compose([transforms.Resize((input_size, input_size)),
+                                                    transforms.ToTensor(),
+                                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                                    ]),
+                      labels=train_labels),
         batch_size=batch_size,
         shuffle=True,
         num_workers=num_workers,
         pin_memory=True)
-    return dataloader
+
+    val_dataloader = DataLoader(
+        AIRushDataset(image_dir, val_meta_data, label_path=train_label_path_,
+                      transform=transforms.Compose([transforms.Resize((input_size, input_size)),
+                                                    transforms.ToTensor(),
+                                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                                    ]),
+                      labels=val_labels),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=1,
+        pin_memory=True)
+
+    return train_dataloader, val_dataloader
 
 
 class AIRushDataset(Dataset):
-    def __init__(self, image_data_path, meta_data, label_path=None, transform=None):
+    def __init__(self, image_data_path, meta_data, label_path=None, transform=None, labels=None):
         self.meta_data = meta_data
         self.image_dir = image_data_path
         self.label_path = label_path
         self.transform = transform
-        
+        self.labels = labels
+
         if self.label_path is not None:
-            self.label_matrix = np.load(label_path)
+            if self.labels is not None:
+                # self.label_matrix = np.load(label_path)
+                self.label_matrix = self.labels
 
     def __len__(self):
         return len(self.meta_data)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.image_dir , str(self.meta_data['package_id'].iloc[idx]) , str(self.meta_data['sticker_id'].iloc[idx]) + '.png')
+        img_name = os.path.join(self.image_dir, str(self.meta_data['package_id'].iloc[idx]), str(self.meta_data['sticker_id'].iloc[idx]) + '.png')
         png = Image.open(img_name).convert('RGBA')
-        png.load() # required for png.split()
+        png.load()  # required for png.split()
 
         new_img = Image.new("RGB", png.size, (255, 255, 255))
-        new_img.paste(png, mask=png.split()[3]) # 3 is the alpha channel
+        new_img.paste(png, mask=png.split()[3])  # 3 is the alpha channel
 
         if self.transform:
             new_img = self.transform(new_img)
-        
+
         if self.label_path is not None:
             tags = torch.tensor(np.argmax(self.label_matrix[idx])) # here, we will use only one label among multiple labels.
             return new_img, tags
         else:
             return new_img
+
--- model.py
+++ model.py
@@ -1,76 +1,47 @@
-from fastai import *
-from fastai.vision import *
+import torch
+import torch.nn as nn
+import torchvision.models as models
 
-def noop(x): return x
 
-class Flatten(nn.Module):
-    def forward(self, x): return x.view(x.size(0), -1)
-
-def conv(ni, nf, ks=3, stride=1, bias=False):
-    return nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=ks//2, bias=bias)
-
-act_fn = nn.ReLU(inplace=True)
-
-def init_cnn(m):
-    if getattr(m, 'bias', None) is not None: nn.init.constant_(m.bias, 0)
-    if isinstance(m, (nn.Conv2d,nn.Linear)): nn.init.kaiming_normal_(m.weight)
-    for l in m.children(): init_cnn(l)
-
-def conv_layer(ni, nf, ks=3, stride=1, zero_bn=False, act=True):
-    bn = nn.BatchNorm2d(nf)
-    nn.init.constant_(bn.weight, 0. if zero_bn else 1.)
-    layers = [conv(ni, nf, ks, stride=stride), bn]
-    if act: layers.append(act_fn)
-    return nn.Sequential(*layers)
-
-class ResBlock(nn.Module):
-    def __init__(self, expansion, ni, nh, stride=1):
+class Baseline(nn.Module):
+    def __init__(self, hidden_size, out_size):
         super().__init__()
-        nf,ni = nh*expansion,ni*expansion
-        layers  = [conv_layer(ni, nh, 3, stride=stride),
-                   conv_layer(nh, nf, 3, zero_bn=True, act=False)
-        ] if expansion == 1 else [
-                   conv_layer(ni, nh, 1),
-                   conv_layer(nh, nh, 3, stride=stride),
-                   conv_layer(nh, nf, 1, zero_bn=True, act=False)
-        ]
-        self.convs = nn.Sequential(*layers)
-        self.idconv = noop if ni==nf else conv_layer(ni, nf, 1, act=False)
-        self.pool = noop if stride==1 else nn.AvgPool2d(2, ceil_mode=True)
-
-    def forward(self, x): return act_fn(self.convs(x) + self.idconv(self.pool(x)))
-
-class XResNet(nn.Sequential):
-    @classmethod
-    def create(cls, expansion, layers, c_in=3, c_out=1000):
-        nfs = [c_in, (c_in+1)*8, 64, 64]
-        stem = [conv_layer(nfs[i], nfs[i+1], stride=2 if i==0 else 1)
-            for i in range(3)]
-
-        nfs = [64//expansion,64,128,256,512]
-        res_layers = [cls._make_layer(expansion, nfs[i], nfs[i+1],
-                                      n_blocks=l, stride=1 if i==0 else 2)
-                  for i,l in enumerate(layers)]
-        res = cls(
-            *stem,
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            *res_layers,
-            nn.AdaptiveAvgPool2d(1), Flatten(),
-            nn.Linear(nfs[-1]*expansion, c_out),
+        self.net = nn.Sequential(
+            # nn.BatchNorm2d(128*128*3),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.5),
+            nn.Conv2d(32, 128, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+            nn.Dropout(0.5),
+            nn.Conv2d(128, 256, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(256),
+            nn.Dropout(0.5),
+            nn.Conv2d(256, 512, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(512),
+            nn.Dropout(0.5),
+            nn.Conv2d(512, 1024, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(1024),
+            nn.Dropout(0.5),
+            nn.Conv2d(1024, out_size, 4, 1),
         )
-        init_cnn(res)
-        return res
 
-    @staticmethod
-    def _make_layer(expansion, ni, nf, n_blocks, stride):
-        return nn.Sequential(
-            *[ResBlock(expansion, ni if i==0 else nf, nf, stride if i==0 else 1)
-              for i in range(n_blocks)])
-
-def xresnet18 (**kwargs): return XResNet.create(1, [2, 2,  2, 2], **kwargs)
-def xresnet34 (**kwargs): return XResNet.create(1, [3, 4,  6, 3], **kwargs)
-def xresnet50 (**kwargs): return XResNet.create(4, [3, 4,  6, 3], **kwargs)
-def xresnet101(**kwargs): return XResNet.create(4, [3, 4, 23, 3], **kwargs)
-def xresnet152(**kwargs): return XResNet.create(4, [3, 8, 36, 3], **kwargs)
+    def forward(self, image):
+        return self.net(image).squeeze(-1).squeeze(-1)
 
 
+class Resnet(nn.Module):
+    def __init__(self, out_size):
+        super().__init__()
+        model = models.resnet18(pretrained=True)
+        model = list(model.children())[:-1]
+        model.append(nn.Conv2d(512, out_size, 1))
+        self.net = nn.Sequential(*model)
+
+    def forward(self, image):
+        return self.net(image).squeeze(-1).squeeze(-1)
