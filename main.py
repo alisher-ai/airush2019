fc06c2d012997a79d365455c11243e67b21922e1
Alisher <alisher.abdulkhaev@gmail.com>
2019-08-14 01:28:12 +0900

    Cross Entropy with weights for each classes due to unbalanced data

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import nsml
 import pandas as pd
 from torchvision import transforms as tr
 from torch.utils.data import Dataset, DataLoader
-from dataloader import train_dataloader
+from dataloader import train_dataloader, normalization
 from dataloader import AIRushDataset
 
 
@@ -38,7 +38,7 @@ def bind_model(model_nsml):
         # DONOTCHANGE This Line
         test_meta_data = pd.read_csv(test_meta_data_path, delimiter=',', header=0)
 
-        input_size = 128  # you can change this according to your model.
+        input_size = 224  # you can change this according to your model.
         batch_size = 200  # you can change this. But when you use 'nsml submit --test' for test infer, there are only 200 number of data.
         device = 0
 
@@ -104,6 +104,7 @@ if __name__ == '__main__':
         last_value += epsilon
         heuristic_vector.append(last_value)
     heuristic_vector = heuristic_vector[::-1]
+    print("Heuristic vector: ", heuristic_vector)
 
     torch.manual_seed(args.seed)
     device = args.device
@@ -114,9 +115,13 @@ if __name__ == '__main__':
     else:
         model = Baseline(args.hidden_size, args.output_size)
     optimizer = optim.Adam(model.parameters(), args.learning_rate)
-    # scheduler = optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.5, last_epoch=-1)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.learning_rate, eta_min=1e-8, last_epoch=-1)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.7, last_epoch=-1)
+    # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.learning_rate, eta_min=1e-8, last_epoch=-1)
 
+    weights = normalization()
+    weights = weights.tolist()
+    print(weights)
+    # criterion = nn.CrossEntropyLoss(weight=torch.DoubleTensor(weights).cuda())  # multi-class classification task
     criterion = nn.CrossEntropyLoss()  # multi-class classification task
     model = model.to(device)
     model.train()
@@ -140,28 +145,35 @@ if __name__ == '__main__':
                 optimizer.zero_grad()
                 image = image.to(device)
                 tags = tags.to(device)
+
                 output = model(image).double()
                 loss = criterion(output, tags)
                 loss.backward()
                 optimizer.step()
 
-                output_prob = F.softmax(output, dim=1)
+                output_prob = F.softmax(output, dim=1)  # batch, ids
+                output_prob = to_np(output_prob)
+                sorted_output_probs = np.sort(output_prob, axis=1)
+                arg_sorted_output_probs = np.argsort(output_prob, axis=1)
+                predict_vector = np.argmax(output_prob, axis=1)
+                predict_vector_2 = arg_sorted_output_probs[:, -2]
+                predict_vector_3 = arg_sorted_output_probs[:, -3]
 
-                sorted_output_probs = np.sort(output_prob)[::-1]
-                arg_sorted_output_probs = np.argsort(output_prob)[::-1]
-
-                predict_vector = np.argmax(to_np(output_prob), axis=1)
-                predict_vector_2 = np.argsort(to_np(output_prob), axis=1)[::-1]
-                predict_vector_3 = np.argsort(to_np(output_prob), axis=1)[::-1]
+                # track the predicted and gt classes
+                if batch_idx % 500 == 0:
+                    print(arg_sorted_output_probs[:10, -1], sorted_output_probs[:10, -1])
+                    print(arg_sorted_output_probs[:10, -2], sorted_output_probs[:10, -2])
+                    print(arg_sorted_output_probs[:10, -3], sorted_output_probs[:10, -3])
+                    print(to_np(tags[:10]))
 
                 label_vector = to_np(tags)
-                bool_vector = predict_vector == label_vector
-                bool_vector_2 = predict_vector_2 == label_vector
-                bool_vector_3 = predict_vector_3 == label_vector
+                bool_sum = sum(1 for a, b in zip(predict_vector, label_vector) if a == b)
+                bool_sum_2 = sum(1 for a, b in zip(predict_vector_2, label_vector) if a == b)
+                bool_sum_3 = sum(1 for a, b in zip(predict_vector_3, label_vector) if a == b)
 
-                accuracy = bool_vector.sum() / len(bool_vector)
-                accuracy_2 = bool_vector_2.sum() / len(bool_vector_2)
-                accuracy_3 = bool_vector_3.sum() / len(bool_vector_3)
+                accuracy = bool_sum / len(label_vector)
+                accuracy_2 = bool_sum_2 / len(label_vector)
+                accuracy_3 = bool_sum_3 / len(label_vector)
 
                 if batch_idx % args.log_interval == 0:
                     print('Batch {} / {}: Batch Loss {:2.4f} / Batch Acc {:2.4f} \t Top-3 Acc: {:2.4f}'.format(batch_idx,
@@ -170,8 +182,8 @@ if __name__ == '__main__':
                                                                                          accuracy,
                                                                                          accuracy + accuracy_2 + accuracy_3))
                 total_loss += loss.item()
-                total_correct += bool_vector.sum()
-                top3_total_correct += bool_vector.sum() + bool_vector_2.sum() + bool_vector_3.sum()
+                total_correct += bool_sum
+                top3_total_correct += bool_sum + bool_sum_2 + bool_sum_3
 
             val_model = copy.deepcopy(model)
             val_model.eval()
@@ -188,26 +200,26 @@ if __name__ == '__main__':
 
                 val_output_prob = F.softmax(val_output, dim=1)
                 val_predict_vector = np.argmax(to_np(val_output_prob), axis=1)
-                val_predict_vector_2 = np.argsort(to_np(val_output_prob), axis=1)[::-1]
-                val_predict_vector_3 = np.argsort(to_np(val_output_prob), axis=1)[::-1]
-
+                val_predict_vector_2 = np.argsort(to_np(val_output_prob), axis=1)[:, -2]
+                val_predict_vector_3 = np.argsort(to_np(val_output_prob), axis=1)[:, -3]
 
                 val_output_prob_heuristic = np.multiply(to_np(val_output_prob), heuristic_vector)
                 val_predict_vector_heuristic = np.argmax(val_output_prob_heuristic, axis=1)
 
                 val_label_vector = to_np(val_tags)
-                val_bool_vector = val_predict_vector == val_label_vector
-                val_bool_vector_2 = val_predict_vector_2 == label_vector
-                val_bool_vector_3 = val_predict_vector_3 == label_vector
+                val_bool_sum = sum(1 for a, b in zip(val_predict_vector, val_label_vector) if a == b)
+                val_bool_sum_2 = sum(1 for a, b in zip(val_predict_vector_2, val_label_vector) if a == b)
+                val_bool_sum_3 = sum(1 for a, b in zip(val_predict_vector_3, val_label_vector) if a == b)
+                val_bool_vector_heuristic_sum = sum(1 for a, b in zip(val_predict_vector_heuristic, val_label_vector) if a == b)
 
-                val_bool_vector_heuristic = val_predict_vector_heuristic == val_label_vector
+                accuracy = val_bool_sum / len(val_label_vector)
+                accuracy_2 = val_bool_sum_2 / len(val_label_vector)
+                accuracy_3 = val_bool_sum_3 / len(val_label_vector)
 
                 val_total_loss += val_loss.item()
-                val_total_correct += val_bool_vector.sum()
-                val_top3_total_correct += val_bool_vector.sum() + val_bool_vector_2.sum() + val_bool_vector_3.sum()
-
-                val_total_correct_heuristic += val_bool_vector_heuristic.sum()
-
+                val_total_correct += val_bool_sum
+                val_top3_total_correct += val_bool_sum + val_bool_sum_2 + val_bool_sum_3
+                val_total_correct_heuristic += val_bool_vector_heuristic_sum
 
             nsml.save(epoch_idx)
             print('TRAIN: \t Epoch {} / {}: Loss {:2.4f} / Epoch Acc {:2.4f}'.format(epoch_idx,
@@ -216,7 +228,7 @@ if __name__ == '__main__':
                                                                            total_correct / len(train_dataloader.dataset),
                                                                            top3_total_correct / len(train_dataloader.dataset)))
 
-            print('VAL: \t Epoch {} / {}: Loss {:2.4f} / Epoch Acc {:2.4f} / Heuristic: {:2.4f}'.format(epoch_idx, args.epochs,
+            print('VAL: \t Epoch {} / {}: Loss {:2.4f} / Epoch Acc {:2.4f} / Heuristic: {:2.4f} / Top-3: {:2.4f}'.format(epoch_idx, args.epochs,
                                                                            val_total_loss / len(val_dataloader.dataset),
                                                                            val_total_correct / len(val_dataloader.dataset),
                                                                            val_total_correct_heuristic / len(val_dataloader.dataset),
